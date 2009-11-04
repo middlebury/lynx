8fb92fea21098a6124a8e1f92df01b8ece11a019
Adam Franco <afranco@middlebury.edu>
2009-11-04 15:20:52 -0500

    Added Application title and basic layout.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -8,6 +8,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 		$this->bootstrap('view');
 		$view = $this->getResource('view');
 		$view->doctype('XHTML1_STRICT');
+		$view->headTitle('Application Title Goes Here');
     }
 
 }
